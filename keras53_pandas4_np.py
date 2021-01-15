ea5090d3f653041ad2b9989a93fe5670208bca4c
ki3123 <ki3123@naver.com>
2021-01-15 09:40:15 +0900

    Add py File [keras53_pandas4_np.py]

---
+++ keras53_pandas4_np.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('../data/csv/iris_sklearn.csv', index_col=0, header=0) #header, index_col =none,0
+
+print(df)
+
+print(df.shape) #(150,)
+print(df.info())
+
+#pandas를 넘파이로 바꾸는 것을 찾아라
+aaa = df.to_numpy
+print(aaa)
+print(type(aaa))
+
+bbb = df.values
+print(bbb)
+print(type(bbb))
+
+np.save('../data/npy/iris_sklearn.npy', arr=aaa)
+
+# 과제 : 판다스의 loc iloc 에 대해 정리 
